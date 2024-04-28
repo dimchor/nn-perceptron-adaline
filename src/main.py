acc72898c5b15cec5c196776d909aaf16ab790f1
aligned64 <113694089+aligned64@users.noreply.github.com>
2024-04-28 18:43:49 +0300

    Added main()

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 import torch
 import perceptron
+import dataset
+import rng
 
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
-)
-print(f"Using {device} device")
+
+def main():
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    print(f"Using {device} device")
+
+    rng.generate_random_values()
+
+if __name__ == "__main__":
+    main()
