e8311b346574035302c658b45c564b48faba8dbf
aligned64 <113694089+aligned64@users.noreply.github.com>
2024-04-29 14:37:06 +0300

    Basic stats on random data

--- src/stats.py
+++ src/stats.py
@@ -2,7 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as sp
 
-def generate_random_values():
+def mean_median_mode(values):
+    return np.mean(values), np.median(values), sp.mode(values)
+
+def variance_stddev(values):
+    return np.var(values), np.std(values)
+
+# demo
+def demo_generate_random_values():
     rng = np.random.default_rng()
     size = 10000
 
@@ -10,6 +17,14 @@ def generate_random_values():
     n = 1000
     p = .5
     values = rng.binomial(n, p, size)
+    mean, median, mode = mean_median_mode(values)
+    print("(arithmetic) mean:", mean)
+    print("median:", median)
+    print("mode:", mode.mode)
+    variance, stddev = variance_stddev(values)
+    print("variance:", variance)
+    print("standard deviation:", stddev)
+
     bins = np.arange(min(values), max(values))
     plt.title("Binomial distribution")
     plt.xlabel("number")
@@ -21,6 +36,14 @@ def generate_random_values():
     mu = 500
     sig = 200
     values = rng.normal(mu, sig, size)
+    mean, median, mode = mean_median_mode(values)
+    print("(arithmetic) mean:", mean)
+    print("median:", median)
+    print("mode:", mode.mode)
+    variance, stddev = variance_stddev(values)
+    print("variance:", variance)
+    print("standard deviation:", stddev)
+
     bins = np.arange(min(values), max(values))
     plt.title("Normal distribution")
     plt.xlabel("number")
